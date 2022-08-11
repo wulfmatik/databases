cdacf5ca57f724fca3662111abd0565cc47259eb
Blake Crawford <blake.crawford@outlook.com>
2022-08-10 20:15:42 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,43 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE user (
+  id INTEGER,
+  username varchar(255),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE messages (
+  id INTEGER,
+  messagebody text,
+  id_user INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_user) REFERENCES user (id)
+);
 
+CREATE TABLE room (
+  id INTEGER,
+  username varchar(255),
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE users_rooms (
+  id INTEGER,
+  id_room INTEGER,
+  id_user INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_room) REFERENCES room (id),
+  FOREIGN KEY (id_user) REFERENCES user (id)
+);
+
+CREATE TABLE rooms_messages (
+  id INTEGER,
+  id_room INTEGER,
+  id_messages INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_room) REFERENCES room (id),
+  FOREIGN KEY (id_messages) REFERENCES messages (id)
+);
 
 
 /*  Execute this file from the command line by typing:
